ce0c829ad54f912dd808b220508446fe224d1641
Eysteinn Örn Jónsson <eysi@live.com>
2024-05-17 12:59:47 +0000

    added an example dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Dashboard } from "@/components/playground-dashboard";
+import "./globals.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Dashboard />
     </div>
   );
 }
